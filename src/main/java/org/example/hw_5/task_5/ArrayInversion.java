36ed6edcee61e66efdb2dc31f6e0585e1ccf6476
Anna-Yavorska <aniayavorsk@gmail.com>
2023-02-08 22:56:53 +0100

    hw_5

--- src/main/java/org/example/hw_5/task_5/ArrayInversion.java
+++ src/main/java/org/example/hw_5/task_5/ArrayInversion.java
@@ -2,18 +2,18 @@ package org.example.hw_5.task_5;
 
 import java.util.Arrays;
 
-// можно было чтуточку проще решить) Но твое решение работает, а это самое главное, поэтому зачтено
 public class ArrayInversion {
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 2, 3, 4};
-        int inversionIndex = array.length - 1;
+        int[] invertedArray = new int[array.length];
+        int index = 0;
 
-        for (int i = 0; i < array.length / 2; i++) {
-            int movingNumber = array[i];
-            array[i] = array[inversionIndex];
-            array[inversionIndex] = movingNumber;
-            inversionIndex--;
+        for (int i = array.length - 1; i > 0; i--) {
+            invertedArray[index] = array[i];
+            index = index + 1;
         }
-        System.out.println(Arrays.toString(array));
+
+        System.out.print(Arrays.toString(array));
+        System.out.print(" -> " + Arrays.toString(invertedArray));
     }
 }
