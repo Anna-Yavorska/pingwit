ed2af2389aa52b0732feb98276a3a552ba2a1f56
Alexander Fomin <RezviyBelorus@gmail.com>
2023-02-20 19:25:51 +0100

    Update LongestSequence.java

--- src/main/java/org/example/hw_5/taskWithAsterisk/LongestSequence.java
+++ src/main/java/org/example/hw_5/taskWithAsterisk/LongestSequence.java
@@ -8,4 +8,43 @@ public class LongestSequence {
 
         finding.find(number);
     }
+    
+    /*
+    public static void main(String[] args) {
+        int maxIndex = 0;
+        int maxCounter = 0;
+
+        int currentIndex = 0;
+        int currentCounter = 1;
+
+        int[] sequence = new int[] {1, 1, 1, 0, 0, 1, 1,1,1};
+        for (int i = 1; i < sequence.length; i++) {
+            int current = sequence[i];
+            int previous = sequence[i - 1];
+
+            if (current == previous) {
+                currentCounter++;
+
+                if (currentCounter == 2) {
+                    currentIndex = i - 1;
+                }
+            } else {
+                if (maxCounter < currentCounter) {
+                    maxCounter = currentCounter;
+                    maxIndex = currentIndex;
+                }
+
+                currentCounter = 1;
+                currentIndex = i;
+            }
+        }
+
+        if (maxCounter < currentCounter) {
+            maxCounter = currentCounter;
+            maxIndex = currentIndex;
+        }
+
+        System.out.println("Количество самой длинной последовательности: " + maxCounter + ", начиная с " + maxIndex + " элемента");
+    }
+    */
 }
