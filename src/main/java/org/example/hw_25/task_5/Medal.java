040b58640aff712b486a968fc399fb8bbe2a4d34
Anna Yavorska <aniayavorsk@gmail.com>
2023-05-01 17:11:49 +0200

    hw_25

--- src/main/java/org/example/hw_25/task_5/Medal.java
+++ src/main/java/org/example/hw_25/task_5/Medal.java
@@ -1,16 +1,7 @@
 package org.example.hw_25.task_5;
 
 public enum Medal {
-    GOLD(1),
-    SIlVER(2),
-    BRONZE(3);
-    private final Integer place;
-
-    Medal(Integer place) {
-        this.place = place;
-    }
-
-    public Integer getPlace() {
-        return place;
-    }
+    GOLD,
+    SIlVER,
+    BRONZE
 }
